43ea7f354890663428232aeb1768999935e3614c
rakitenkogermans <rakitenko.germans@gmail.com>
2023-03-06 20:18:36 +0200

    test ui ok

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,7 +45,8 @@ module.exports = {
         'react-hooks/exhaustive-deps': 'error',
         'max-len': ['error', { code: 120, ignoreComments: true }],
         'react/jsx-tag-spacing': ['error', { beforeSelfClosing: 'proportional-always', beforeClosing: 'proportional-always' }],
-        '@typescript-eslint/no-dynamic-delete': 'off'
+        '@typescript-eslint/no-dynamic-delete': 'off',
+        '@typescript-eslint/no-misused-promises': 'off'
     },
     overrides: [
         {
