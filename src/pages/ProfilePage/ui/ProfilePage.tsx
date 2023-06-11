f626a496f8b8535422e311606c1dda7e7a414a1c
rakitenkogermans <rakitenko.germans@gmail.com>
2023-06-11 20:52:41 +0300

    add generate slice script, eslintignore

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,5 +1,5 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { memo, useCallback, useEffect } from 'react';
+import { memo, useCallback } from 'react';
 import {
     DynamicModuleLoader,
     type ReducersList,
