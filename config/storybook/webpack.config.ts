bbc6d35625329e2dfb316c4cdf9f67459c31dcf6
rakitenkogermans <rakitenko.germans@gmail.com>
2023-07-11 13:29:34 +0300

    fix storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -24,6 +24,10 @@ export default ({ config }: { config: Configuration }) => {
 
     config.resolve?.modules?.push(paths.src);
     config.resolve?.extensions?.push('.ts', '.tsx');
+    config!.resolve!.alias = {
+        ...config!.resolve!.alias,
+        '@': paths.src,
+    };
 
     if (config.module) {
         // @ts-ignore
--- src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
+++ src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { type ComponentMeta, type ComponentStory } from '@storybook/react';
 
 import { NotificationList } from './NotificationList';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
 export default {
     title: 'entities/Notification/NotificationList',
     component: NotificationList,
+    decorators: [StoreDecorator({})],
     argTypes: {
         backgroundColor: { control: 'color' },
     },
@@ -15,3 +17,29 @@ const Template: ComponentStory<typeof NotificationList> = (args) => <Notificatio
 
 export const Normal = Template.bind({});
 Normal.args = {};
+Normal.parameters = {
+    mockData: [
+        {
+            url: `${__API__}/notifications`,
+            method: 'GET',
+            status: 200,
+            response: [
+                {
+                    id: '1',
+                    title: 'Notification 1',
+                    description: 'Something interesting happened',
+                },
+                {
+                    id: '2',
+                    title: 'Notification 2',
+                    description: 'Something interesting happened',
+                },
+                {
+                    id: '3',
+                    title: 'Notification 3',
+                    description: 'Something interesting happened',
+                },
+            ],
+        },
+    ],
+};
