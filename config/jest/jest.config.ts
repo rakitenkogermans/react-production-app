efa0c1454ba730b03c71c012704f82c4a5eb3235
rakitenkogermans <rakitenko.germans@gmail.com>
2023-07-19 11:49:16 +0300

    add report to ci

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -27,7 +27,7 @@ export default {
             {
                 publicPath: '<rootDir>/reports/unit',
                 filename: 'report.html',
-                openReport: true,
+                // openReport: true,
                 inlineSource: true,
             },
         ],
